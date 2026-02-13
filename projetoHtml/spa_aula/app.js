33ac90423d6cc570df3b711767582b34d1eabd8c
rodrigopereiradevelopment <rodrigopereira.development@gmail.com>
2026-02-13 19:09:21 -0300

    update

--- projetoHtml/spa_aula/app.js
+++ projetoHtml/spa_aula/app.js
@@ -13,11 +13,11 @@ const pages = {
         <section class="card">
         <h2>Alunos</h2>
         <ul>
-        ${alunos.map((a)=> `<li> ${a}<?li>`).join("")}
+        ${alunos.map((a)=> `<li> ${a}</li>`).join("")}
         </ul>
         </section>
-    `;
-    }
+    `
+    },
     "/sobre": () =>`
     <section class="card">
     <h2>Sobre</h2>
@@ -26,3 +26,30 @@ const pages = {
     </section>
     `,
 };
+
+const router = () => {
+    // 1. Descobre em qual página estamos
+    const path = window.location.pathname;
+    
+    // 2. Busca o conteúdo ou exibe o "Início" se a rota não existir
+    const render = pages[path] || pages["/"];
+    
+    // 3. Injeta o HTML no elemento <main id="app">
+    app.innerHTML = render();
+};
+
+// Escuta cliques no documento inteiro
+document.addEventListener("click", e => {
+    // Se o clique foi em um link com o atributo [data-link]
+    if (e.target.matches("[data-link]")) {
+        e.preventDefault(); // Não recarrega a página
+        history.pushState(null, null, e.target.href); // Muda a URL
+        router(); // Atualiza o conteúdo da tela
+    }
+});
+
+// Faz o botão "Voltar" do navegador funcionar
+window.addEventListener("popstate", router);
+
+// Renderiza a página assim que o site abre pela primeira vez
+router();
