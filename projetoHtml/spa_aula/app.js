11252f1377ff2b8a07ee8ffc64a75403566fbb46
Rodrigo Pereira <rodrigopereira.development@gmail.com>
2026-02-12 22:31:59 -0300

    update js

--- projetoHtml/spa_aula/app.js
+++ projetoHtml/spa_aula/app.js
@@ -6,17 +6,23 @@ const pages = {
 <h2>Inicio</h2>
 <p>Isso é uma SPA : navegação sem recarregar a página</p>
 <p>Use o menu para trocar de página</p>
-</section>
 `,
-"/alunos" : () => {
-    const alunos = ["Ana","Bruno","Carlos","Diana"];
-    return `
-    <section class="card">
-    <h2>Alunos</h2>
-    <ul>
+    "/alunos" : () => {
+        const alunos = ["Ana","Bruno","Carlos","Diana"];
+        return `
+        <section class="card">
+        <h2>Alunos</h2>
+        <ul>
         ${alunos.map((a)=> `<li> ${a}<?li>`).join("")}
-    </ul>
+        </ul>
+        </section>
+    `;
+    }
+    "/sobre": () =>`
+    <section class="card">
+    <h2>Sobre</h2>
+    <h2>Exemplo simples de roteamento do flont-end (sem framework).</h2>
+    <p>Próximo passo:carregar dados via fetch e criar componentes.</p>
     </section>
-    `
-}
-}
+    `,
+};
