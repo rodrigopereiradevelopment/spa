2dec1f7a4c65dc16f1a04499189e2fe13db8e739
Rodrigo Pereira <rodrigopereira.development@gmail.com>
2026-02-12 22:20:46 -0300

    js

--- projetoHtml/spa_aula/app.js
+++ projetoHtml/spa_aula/app.js
@@ -1,5 +1,22 @@
 const app =document.getElementById("app");
 
 const pages = {
-    
+"/": () => `
+<section class="card">
+<h2>Inicio</h2>
+<p>Isso é uma SPA : navegação sem recarregar a página</p>
+<p>Use o menu para trocar de página</p>
+</section>
+`,
+"/alunos" : () => {
+    const alunos = ["Ana","Bruno","Carlos","Diana"];
+    return `
+    <section class="card">
+    <h2>Alunos</h2>
+    <ul>
+        ${alunos.map((a)=> `<li> ${a}<?li>`).join("")}
+    </ul>
+    </section>
+    `
+}
 }
